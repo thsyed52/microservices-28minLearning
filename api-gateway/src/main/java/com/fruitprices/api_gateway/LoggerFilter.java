b0a5efcf3a54f104f079f9fbc4fc3e1db0281408
thsyed52 <thsyed52@yahoo.com>
2024-09-30 19:35:21 -0400

    LoggerFilter.java added in Api Gateway

--- api-gateway/src/main/java/com/fruitprices/api_gateway/LoggerFilter.java
+++ api-gateway/src/main/java/com/fruitprices/api_gateway/LoggerFilter.java
@@ -1,2 +1,22 @@
-package com.fruitprices.api_gateway;public class LoggerFilter {
+package com.fruitprices.api_gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+@Component
+public class LoggerFilter implements GlobalFilter {
+
+    private Logger logger = LoggerFactory.getLogger(LoggerFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("path of request -> {} " , exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
 }
